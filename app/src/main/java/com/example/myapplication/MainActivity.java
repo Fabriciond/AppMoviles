bedd7332204b7f855f5e0afb9aedbcd6e28b42fa
Fabriciond <78011988+Fabriciond@users.noreply.github.com>
2023-02-10 09:17:09 -0700

    carga inicial

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.myapplication;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        (findViewById(R.id.btnAceptar)).setOnClickListener(this);
+        //ya lo subi
+    }
+
+    @Override
+    public void onClick(View v) {
+        String nombre = ((TextView)findViewById(R.id.txtNombre)).getText().toString();
+        Toast.makeText(getApplicationContext(),"hola "+nombre+" bienvenido a mi app",Toast.LENGTH_LONG).show();
+
     }
 }
